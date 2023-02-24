7be0f2c6e5ec80e29d400fd3e594790bbb0f7040
Tayo Ademolu <12128516+tymolls@users.noreply.github.com>
2023-02-24 12:37:53 +0000

    Initial Commit

---
+++ Classes/Phpdocx/Factory/Phpdocx_Phpdocx.php
@@ -0,0 +1,17 @@
+<?php
+namespace Phpdocx\Factory;
+use Phpdocx\Create\CreateDocx;
+/**
+ * Class to use PHPDocX within a namespace
+ *
+ * @category   Phpdocx
+ * @package    factory
+ * @copyright  Copyright (c) Narcea Producciones Multimedia S.L.
+ *             (http://www.2mdc.com)
+ * @license    phpdocx LICENSE
+ * @link       http://www.phpdocx.com
+ */
+class Phpdocx_Phpdocx extends CreateDocx
+{
+    
+}
